6a88f2f1632b569351e5da1f7306f3353af0f698
odulaja philip <odulajaphilip@gmail.com>
2022-12-28 19:46:17 +0100

    completed section 02

--- Section 01- Python print function and string manipulation/coding_challenge/challange_one.py
+++
@@ -1,9 +0,0 @@
-# Challenge Instruction 
-
-# challenge 1 : Print your name to the console 
-# challenge 2 : Print the sentence I would be the best python programmer and i would be so good at it !
-# challenge 3 : Print the sentence I'll complete this course and never quit on my dreams
-
-# write your code below
-# Note: each statement should be on different line i.e use different print() functin for each
-# You absolutely got this!
--- Section 01- Python print function and string manipulation/coding_challenge/challenge_two.py
+++
@@ -1,7 +0,0 @@
-# The code below is error prone and needs debugging 
-# Debug the code below using your previous knownledge of debugging and try checking stackoverflow for similar error solutions
-
-# print("Programming is fun)"
-
-# print("Python seems easier than i ever thought\nThis is a new line "/nAnd another line)
-#     print("Debugging completed!!")
