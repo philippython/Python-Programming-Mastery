07fb746ac580c3c6bd76b567b9119c54c8a4478b
odulaja philip <odulajaphilip@gmail.com>
2022-12-29 23:16:55 +0100

    renamed section 02

---
+++ Section 02 Introduction to Python Programming/final code/main.py
@@ -0,0 +1,165 @@
+# name = "philip"
+# example_of_string = "59087kfjghkl;f909-447-=3=3=7"
+# first_name = "philip"
+# second_name = "odulaja"
+# age = "18"
+
+# # the variable fname is combination of first_name and second_name using f-string 
+# full_name = first_name + " " + second_name + "\n" + age
+# fname = f"{first_name} {second_name}"
+# description = f"I am {first_name} {second_name} a python engineer and instructor"
+
+# print(description)
+
+# ctrl + / on windows   command + / on mac
+ 
+# docstring 
+""" 
+this is an example of a docstring
+docstring is used for multiline commenting
+"""
+
+# python primitive data types
+# string
+# integer
+# float
+# boolean 
+# currency_of_the_US = "USD"
+# datatype_name = type(currency_of_the_US)
+# print(datatype_name)
+
+# integers are natural numbers without floating points
+# avg_income = 350
+# datatype_name_of_income = type(avg_income)
+# print(datatype_name_of_income)
+
+#  floating point numbers
+# interest_rate = 5.78
+# type_interest = type(interest_rate)
+# print(type_interest)
+
+# boolean True and False
+#  true
+# is_snowing = False
+# type_is_snowing = type(is_snowing)
+# print(type_is_snowing)
+
+# datatype conversion or python casting
+# age_string = "18"
+# type_of_age = type(age_string)
+# print(type_of_age)
+
+# python casting or datatype conversion 
+#  string -> integer
+# age_int = int(age_string)
+# type_of_age_int = type(age_int)
+# print(type_of_age_int)
+
+# cost = 500
+# integer -> string
+# cost_string = str(cost)
+# print(cost_string)
+# type_of_cost_string  = type(cost_string)
+# print("cost_string is of type", type_of_cost_string )
+
+# float -> integer
+# interest = 12.9834
+# interest_int = int(interest)
+# print(interest_int)
+
+# integer -> float
+# fees = 1000
+# fees_float = float(fees)
+# print(fees_float)
+
+# programming_lng = input("What is your favorite programming language?")
+# print(type(programming_lng))
+# print(programming_lng)
+
+# age = input("How old are you? \n")
+# age_int = int(age)
+# print(type(age))
+# print(type(age_int))
+# print(age)
+
+# is_good = input("IS this python course great? True or False")
+# print(type(is_good))
+# print(is_good)
+name = input("what is your name? ")
+dream_company = input("What is the name of your dream company? ")
+programming_lng = input("what is your favorite programming language? ")
+# I am philip, i wish to work at amazon and my favorite programming language is python
+# description = "I am " + name + ", i wish to work at " + dream_company + "and my favorite programming language is " + programming_lng
+description = f"I am {name}, i wish to work at {dream_company} and my favorite programming language is {programming_lng} "
+# string concatenation
+print(description)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
