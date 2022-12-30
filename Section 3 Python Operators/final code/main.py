618829107eea2617d8c44b9a948e2405c68f9283
odulaja philip <odulajaphilip@gmail.com>
2022-12-30 21:49:59 +0100

    created my first whiteboard section

---
+++ Section 3 Python Operators/final code/main.py
@@ -0,0 +1,182 @@
+#  Arithnetic operators
+# + , * , / , - , // , ** , %
+bill = 5_000
+tax = 200
+salary = 100_000
+# print(salary)
+
+# addition operation
+# total = bill + tax
+# print(total)
+
+# substraction operator
+# differnce = bill - tax
+# print(differnce)
+
+# multiplication operation
+# multiply = bill * tax
+# print(multiply)
+
+# division operation 
+#  5000 / 200
+# dividend = bill / tax
+# print(dividend)
+
+#  floor division operation
+# 5000 // 200
+# floor_dividend = bill //  tax
+# print(floor_dividend)
+
+
+# print(7 // 2)
+# 3.5
+
+# modulus operation
+#  % 
+# 5000 % 200
+# 5000 / 200 = 25 
+# fee = 5734
+
+
+# mod_remain = fee % 200
+# mod_divide = fee / 200
+# print(mod_divide)
+# print(mod_remain)
+# 5734 / 200 = 28 remain 134
+# 134 / 200
+# modulus = bill % tax
+# print(modulus)
+
+
+# exponential operator
+#  **
+# initial = 9
+# power = 3
+# expo = initial ** power
+# 9 ** 2 = 9 * 9 * 9
+# print(expo)
+
+# assignment operators
+loan = 5700 
+# equal to 
+
+# +=
+loan = loan + 300
+# print(loan)
+
+#  plus equal
+loan += 300
+# print(loan)
+
+# substract equal -=
+# loan -= 500
+# print(loan)
+
+# multiply equal
+loan *= 2
+# 5800 * 2
+# print(loan)
+
+# # divide equal 
+# loan /= 2
+# print(loan)
+
+
+# floor divide equal
+# loan //= 2
+# print(loan)
+
+loan = 70_000
+repayment = 80_000
+
+# comparisom operators
+#  == , != , > , < , >= , <=
+
+# equal equal ==
+bill = 500
+response = "yes"
+
+# not equal !=
+# if response != "yes":
+#     print("User rejected admission")
+# else:
+#     print("User accepted admission")
+
+# <= is combination of == and <
+# if loan <= repayment:
+#     print("User has completed repaying loan")
+# else:
+#     print("User is a debtor")
+available_bullets = 12
+
+# >= combination of > and ==
+
+# if available_bullets >= 1 :
+#     print("Keep shooting")
+# else:
+#     print("reloading...")
+
+# if response == "no":
+#     print("User Response is NO")
+# else:
+#     print("User response is yes")
+
+
+# if bill == 500 :
+#     print("Bill is accurate")
+# else:
+#     print("Bill is not accurate")
+
+# if loan > repayment :
+#     print("User is still owing")
+# else:
+#     print("Loan is repaid")
+
+
+score = 64 
+
+if score >= 70 :
+    print("Grade A")
+elif score >= 60 : 
+    print("Grade B")
+elif score >= 50 :
+    print("Grade C")
+else:
+    print("Grade D")
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
