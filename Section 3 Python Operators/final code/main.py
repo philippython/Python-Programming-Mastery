2c02d63bba6e4c03eba21a74a9dbaadf73cfdd06
odulaja philip <odulajaphilip@gmail.com>
2023-01-03 22:15:25 +0100

    completed section 3

--- Section 3 Python Operators/final code/main.py
+++ Section 3 Python Operators/final code/main.py
@@ -188,33 +188,78 @@ available_bullets = 12
 #     else:
 #         print("Number is positive")
 
-is_hungry = input("Are you hungry? ")
+# is_hungry = input("Are you hungry? ")
 
-if is_hungry == "yes" :
-    print("Go to grocery store")
-    # inner if..else condition 
-    cost = float(input("How much does a chocolate cost ?"))
-    if cost <= 1 :
-        print("Buy 3 chocolates")
-    else :
-        print("Buy 1 choclate")
-elif is_hungry == "no":
-    print("Stay at home")
-    interested_in_game = input("Do you want to play fortnite? ")
-    # first inner if.. else conditional statement
-    if interested_in_game == "yes":
-        print("See you next week")
-    elif interested_in_game == "no":
-        print("Go back to study")
-        # creating another input function
-        fav_subject = input("What is your favorite subject? ")
-        # inner if..else statement conditional statement
-        if fav_subject == "computer science" :
-            print("That is my favorite subject")
-        else : 
-            print("We have nothing in common")
-else:
-    print("Invalid Reponse")
+# if is_hungry == "yes" :
+#     print("Go to grocery store")
+#     # inner if..else condition 
+#     cost = float(input("How much does a chocolate cost ?"))
+#     if cost <= 1 :
+#         print("Buy 3 chocolates")
+#     else :
+#         print("Buy 1 choclate")
+# elif is_hungry == "no":
+#     print("Stay at home")
+#     interested_in_game = input("Do you want to play fortnite? ")
+#     # first inner if.. else conditional statement
+#     if interested_in_game == "yes":
+#         print("See you next week")
+#     elif interested_in_game == "no":
+#         print("Go back to study")
+#         # creating another input function
+#         fav_subject = input("What is your favorite subject? ")
+#         # inner if..else statement conditional statement
+#         if fav_subject == "computer science" :
+#             print("That is my favorite subject")
+#         else : 
+#             print("We have nothing in common")
+# else:
+#     print("Invalid Reponse")
+
+
+# solution to football admin project
+# match_status = input("What is the status of the match? ")
+
+# if match_status == "suspended" :
+#     print("Match suspended")
+# elif match_status == "played":
+#     home_goals = int(input("How many goals was scored by the home team? "))
+#     away_goals = int(input("How many goals was scored by the away team? "))
+#     if home_goals > away_goals:
+#         print("Home team won the match")
+#     elif home_goals == away_goals :
+#         print("The match was a draw")
+#     else:
+#         print("Away team won the match")
+# else:
+#     print("Invalid match status")
+
+# minutes to hours converter
+minutes = float(input("How many minutes would you like to convert to hours"))
+
+# floor divide 
+hours = minutes // 60 # 372.5 // 60 = 6.325
+mins = minutes % 60 # 372.5 / 60 372.5 / 60 = 6
+# 60 * 6 = 360 12.5 
+
+
+response = f"{hours}hrs {mins}mins"
+print(response)
+# float datatype
+# 360mins -> 6 hrs
+# 372.5 mins -> 6 hrs ...mins
+# 6 hrs 30mins 
+
+
+
+
+
+
+
+
+
+
+
 
 
 
