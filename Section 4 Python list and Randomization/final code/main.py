5e3865479f2e7c9a0007fa907c750f82dd802e32
odulaja philip <odulajaphilip@gmail.com>
2023-01-08 20:21:43 +0100

    added python list lecture

--- Section 4 Python list and Randomization/final code/main.py
+++ Section 4 Python list and Randomization/final code/main.py
@@ -1,11 +1,55 @@
 # import bank
 # import modulename
 
-from bank import  all_users as alu
-
+# from bank import  all_users as alu
 # renamed the varibale name to alu
 
-all_users = ["thomas", "chris", "shadrach"]
+# all_users = ["thomas", "chris", "shadrach"]
 
-print(alu)
-print(all_users)
+# print(alu)
+# print(all_users)
+
+import random
+import bank 
+import string 
+# from bank import all_users
+
+# random.shuffle(scores)
+# print(scores)
+
+# seed function
+# random.seed(7) # seed value
+# winner = random.choice(names)
+# print(winner)
+# random_votes = random.randint(70, 100)
+# print(random_votes)
+
+#  choice , random , randint , shuffle, randbytes , seed
+# random_score = random.choice(scores)
+# print(random_score)
+# iterable
+
+# random_float = random.random()
+# print(random_float)
+
+# random_integer = random.randint(4, 10)  # 4 -> 10
+# print(random_integer)
+
+# creating a list
+# first_list = []
+# print(type(first_list))
+
+first_list = list()
+print(first_list)
+
+class_list = ["chris", "philip", "thomas", "shadrach"] # positive indexing -> [0 , 1 , 2 , 3]
+#            negative indexing - > [-4 ,-3,-2,-1]
+# accessing items in a list 
+first_item  = class_list[0]
+print(first_item)
+# indexing 
+
+last_item = class_list[-1]
+print(last_item)
+
+
