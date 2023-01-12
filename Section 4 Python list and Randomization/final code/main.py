a904005681c9cbc4622c3a2becce296cd5ba48e2
odulaja philip <odulajaphilip@gmail.com>
2023-01-13 00:53:39 +0100

    created content about len function and indexerror

--- Section 4 Python list and Randomization/final code/main.py
+++ Section 4 Python list and Randomization/final code/main.py
@@ -39,17 +39,29 @@ import string
 # first_list = []
 # print(type(first_list))
 
-first_list = list()
-print(first_list)
+# first_list = list()
+# print(first_list)
 
-class_list = ["chris", "philip", "thomas", "shadrach"] # positive indexing -> [0 , 1 , 2 , 3]
+# class_list = ["chris", "philip", "thomas", "shadrach"] # positive indexing -> [0 , 1 , 2 , 3]
 #            negative indexing - > [-4 ,-3,-2,-1]
 # accessing items in a list 
-first_item  = class_list[0]
-print(first_item)
+# first_item  = class_list[0]
+# print(first_item)
 # indexing 
 
-last_item = class_list[-1]
-print(last_item)
+programming_lngs = ["Java", "C++", "Javascript", "Python", "C", "C#"] # [0, 1 , 2, 3, 4, 5] 6 - 1
+first_item = programming_lngs[0] 
+fourth_item = programming_lngs[3]
+print(first_item)
+print(fourth_item)
 
+# position on list - 1 
+#  firstpostion = 1   sixthposition = 6
+# 1- 1 = 0   6 -1 = 5
+# len function
+total_lngs = len(programming_lngs)
+print(total_lngs)
 
+# indexerror
+item  = programming_lngs[10]
+print(item)
