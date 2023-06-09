f6415aac8c22aeaac862d29419420536ec878679
Odulaja Philip temitayo <100697141+philippython@users.noreply.github.com>
2023-06-09 16:05:22 +0100

    initial commit

--- Section 8 Python Dictionary and Arbitary Arguments/final code/main.py
+++ Section 8 Python Dictionary and Arbitary Arguments/final code/main.py
@@ -80,46 +80,86 @@
 # students_1 = {"philip": 4.14, "hassan": 4.23}
 # students_2 = {"jamiu": 2.78, "farouq": 3.00}
 
-fruit_categories = {
-    'fruit': {
-        'apple': {
-            'color': 'red',
-            'taste': 'sweet'
-        },
-        'banana': {
-            'color': 'yellow',
-            'taste': 'sweet'
-        }
-    },
-    'vegetable': {
-        'carrot': {
-            'color': 'orange',
-            'taste': 'crunchy'
-        },
-        'spinach': {
-            'color': 'green',
-            'taste': 'bitter'
-        }
-    }
-}
+# fruit_categories = {
+#     'fruit': {
+#         'apple': {
+#             'color': 'red',
+#             'taste': 'sweet'
+#         },
+#         'banana': {
+#             'color': 'yellow',
+#             'taste': 'sweet'
+#         }
+#     },
+#     'vegetable': {
+#         'carrot': {
+#             'color': 'orange',
+#             'taste': 'crunchy'
+#         },
+#         'spinach': {
+#             'color': 'green',
+#             'taste': 'bitter'
+#         }
+#     }
+# }
 
 # Spinach is a vegetable it has a bitter taste and it's green in color
 
-def describe_fruit(fruit_name: str, fruits: dict) -> str:
+# def describe_fruit(fruit_name: str, fruits: dict) -> str:
 
-    for category, values in fruits.items():
-        for fruit, property in values.items():
-            if fruit_name == fruit:
-                color = property.get("color")
-                taste = property.get("taste")
-                print(f"{fruit_name} is a {category} it has a {taste} taste and it's {color} in color")
+    # for category, values in fruits.items():
+    #     for fruit, property in values.items():
+    #         if fruit_name == fruit:
+    #             color = property.get("color")
+    #             taste = property.get("taste")
+    #             print(f"{fruit_name} is a {category} it has a {taste} taste and it's {color} in color")
             
 
-describe_fruit("carrot", fruit_categories)
-describe_fruit("apple", fruit_categories)
-describe_fruit("banana", fruit_categories)
-describe_fruit("spinach", fruit_categories)
-describe_fruit("orange", fruit_categories)
+# describe_fruit("carrot", fruit_categories)
+# describe_fruit("apple", fruit_categories)
+# describe_fruit("banana", fruit_categories)
+# describe_fruit("spinach", fruit_categories)
+# describe_fruit("orange", fruit_categories)
+
+# Arbitrary positional arguments
+# def calculate_average_score(*students):
+#     sum = 0
+#     for score in students:
+#         sum += score
+#         no_of_students = len(students)
+#         average = sum / no_of_students
+#     print(average)
+
+    
+# calculate_average_score()
+
+# def process_data(*args, **kwargs):
+#     for num in args:
+#         print(num)
+
+#     for key, value in kwargs.items():
+        # print(f"{key}: {value}")
+
+# Example usage
+# process_data(name="John", age=30, city="New York")
+# process_data(90, 89, 90, language="Python", framework="Django", database="PostgreSQL")
+
+def count_word(sentence, *words):
+    # tokenizing a sentence 
+    # The boy is a good boy
+    # ["The", "boy", "is", "a", "good",  "boy"]
+    word_tokenization  = sentence.split()
+    word_dict = {}
+
+    for word in words:
+        word_count = word_tokenization.count(word)
+        word_dict[word] = word_count
+
+    return word_dict 
+
+job_description = "Senior Python Developer with experience with Python and Django framework"
+count_word_result = count_word(job_description, "Python", "Senior", "with", "experience")
+print(count_word_result)
 
 
 
@@ -149,11 +189,63 @@ describe_fruit("orange", fruit_categories)
 
 
 
-# merge 1: 
-# students_1.update(students_2)
-# print(students_1)
 
-# merge 2:
-# total_students = {**students_1, **students_2}
-# print(total_students)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
