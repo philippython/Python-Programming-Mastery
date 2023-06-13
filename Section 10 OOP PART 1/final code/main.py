3be2943e4d6766fc981b8f0d3129452ff1e4ffe7
Odulaja Philip temitayo <100697141+philippython@users.noreply.github.com>
2023-06-13 22:27:25 +0100

    initial commit

---
+++ Section 10 OOP PART 1/final code/main.py
@@ -0,0 +1,65 @@
+#  creating/defining a class
+# class Human:
+#     def __init__(self, sex, skin_color, weight):
+#         # attributes of class
+#         self.sex = sex
+#         self.skin_color = skin_color
+#         self.weight = weight
+
+#     def display_information(self):
+#         print(f"This human is a {self.sex} and is {self.skin_color} and weighs {self.weight}kg at birth")
+
+#     def __str__(self) -> str:
+#         # string representation of a class object
+#         return f"Human(sex, skin_color, weight)"
+
+# # creating Human objects
+# philip = Human("male", "black", 2.0)
+# mary = Human("female", "white", 2.5)
+
+# weightPhilip = philip.weight
+# philip.display_information()
+# mary.display_information()
+
+# print(mary.__str__())
+# print(weightPhilip)
+# print(type(philip))
+# print(type(usman))
+
+
+class Person:
+
+    def __init__(self, name, age, occcupation):
+        self.age = age
+        self.name = name
+        self.occupation = occcupation
+
+    def introduce(self):
+        print(f"My name is {self.name} i am {self.age} yrs old and My occupation is {self.occupation}") 
+
+    def change_occupation(self, new_occupation):
+        self.occupation = new_occupation          
+
+caroline = Person("Caroline Jessica", 27, "Sailor")
+philip = Person("Odulaja Philip", 19, "Python Engineer and Instructor")
+
+caroline.introduce()
+philip.introduce()
+
+caroline.change_occupation("Teacher")
+print(caroline.occupation)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Section 10 OOP PART 1/starter code/main.py
@@ -0,0 +1,11 @@
+# Create a class called "Person" with the following properties:
+# - name (string)
+# - age (integer)
+# - occupation (string)
+# The class should have a constructor that initializes these properties.
+
+# Create two methods within the class:
+# - introduce(): prints a message introducing the person with their name, age, and occupation.
+# - change_occupation(new_occupation): updates the occupation of the person.
+
+# Create two objects of the Person class and test the methods.
