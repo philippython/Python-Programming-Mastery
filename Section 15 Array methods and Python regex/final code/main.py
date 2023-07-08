f4ea18b6a6016800306c121fb0e6b8050bca6de9
Odulaja Philip temitayo <100697141+philippython@users.noreply.github.com>
2023-07-08 08:02:13 +0100

    initial commit

---
+++ Section 15 Array methods and Python regex/final code/main.py
@@ -0,0 +1,41 @@
+# lambda parameters : expression 
+# simple_interest = lambda p , r , t : p * r * t / 100
+
+# print(simple_interest(4500, 5, 2))
+
+# def discount_calculator_percentage(discount_percentage: float):
+#         shoppingmall_name = "walmart"
+
+#         return lambda items : f"You have enjoyed a discount of {sum(items) * discount_percentage / 100} the total cost is {sum(items)} and the discounted cost is {sum(items) - (sum(items) * discount_percentage / 100)} \nThanks for shopping at {shoppingmall_name}"
+        # def calculate_discount_price(items: List):
+        #     total = sum(items) 
+        #     discount_amount = total * discount_percentage / 100
+        #     discounted_cost = total - discount_amount
+        #     print(f"You have enjoyed a discount of {discount_percentage}, the total cost is {total} and the discounted cost is {discounted_cost}")
+        #     print(f"Thanks for chossing {shoppingmall_name}")
+        #     return discounted_cost
+        
+        # return calculate_discount_price
+        
+# cart = [67.90, 678, 450, 34]
+# discount_percent = discount_calculator_percentage(10)
+# print(discount_percent(cart))
+# discount_ten_percent = discount_calculator_percentage(10)
+# discount_price_on_10 = discount_ten_percent(cart)
+# print(discount_price_on_10)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
