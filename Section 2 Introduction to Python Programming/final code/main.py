5756506b1d276a31b166c6a018c3f1043a9c269b
Odulaja Philip temitayo <100697141+philippython@users.noreply.github.com>
2023-08-14 19:35:46 +0100

    initial commit

--- Section 2 Introduction to Python Programming/final code/main.py
+++ Section 2 Introduction to Python Programming/final code/main.py
@@ -1,76 +1,122 @@
-# name = "philip"
-# example_of_string = "59087kfjghkl;f909-447-=3=3=7"
-# first_name = "philip"
-# second_name = "odulaja"
-# age = "18"
+''' 
+Docstring 
+# ctrl + / command + /
+# goes in front of it
+# this program focuses on python basics
+# docstring is used for making multiline comments
 
-# # the variable fname is combination of first_name and second_name using f-string 
-# full_name = first_name + " " + second_name + "\n" + age
-# fname = f"{first_name} {second_name}"
-# description = f"I am {first_name} {second_name} a python engineer and instructor"
+'''
+# print("helloworld!")
+# print('Hello this is my first python program')
+# print('This is a test')
 
-# print(description)
+# course_title = "Python Programming Mastery"
+# title2 = "PPM"
+# advanced_course_2 = "Django course"
+# new_line = "I am a text on a new line"
+# # &Symbol = "Symbol"
 
-# ctrl + / on windows   command + / on mac
- 
-# docstring 
-""" 
-this is an example of a docstring
-docstring is used for multiline commenting
-"""
+# # string manipulation
+# # string concatenation
+# complete_course = course_title + " " + title2 + "\n" + new_line
+# # seperator = course_title - title2
+# print(complete_course)
+# # print(seperator)
+
+# name = "Philip"
+# age = "19"
+# role = "Python engineer and instructor"
+
+# # f-string
+# # info = "My name is " + name + "I'm " + age + " yrs old" + "a " + role
+# info_with_f_string = f"My name is {name} I'm {age} yrs old a {role}"
+# # print(info)
+# print(info_with_f_string)
 
 # python primitive data types
 # string
 # integer
 # float
 # boolean 
-# currency_of_the_US = "USD"
-# datatype_name = type(currency_of_the_US)
-# print(datatype_name)
 
-# integers are natural numbers without floating points
-# avg_income = 350
-# datatype_name_of_income = type(avg_income)
-# print(datatype_name_of_income)
+python_inventor = "Guido van rossum"
+integer = 19_000_000
+type_of_integer = type(integer)
+type_of_string = type(python_inventor)
 
-#  floating point numbers
-# interest_rate = 5.78
-# type_interest = type(interest_rate)
-# print(type_interest)
+bill = 90.99
+type_of_bill = type(bill)
+print(integer)
 
-# boolean True and False
-#  true
-# is_snowing = False
-# type_is_snowing = type(is_snowing)
-# print(type_is_snowing)
+is_teenager = False
+# boolean = type(is_teenager)
+# print(boolean)
 
-# datatype conversion or python casting
-# age_string = "18"
-# type_of_age = type(age_string)
-# print(type_of_age)
+'''
+type function in python is used in checking the type of an object
+integer is any natural number without floating points
+true false in javasvcript
 
-# python casting or datatype conversion 
-#  string -> integer
-# age_int = int(age_string)
-# type_of_age_int = type(age_int)
-# print(type_of_age_int)
+'''
 
-# cost = 500
-# integer -> string
-# cost_string = str(cost)
-# print(cost_string)
-# type_of_cost_string  = type(cost_string)
-# print("cost_string is of type", type_of_cost_string )
+# python casting / datatype coversion / type casting
 
-# float -> integer
-# interest = 12.9834
-# interest_int = int(interest)
-# print(interest_int)
+twitter_ceo = "Elon musk"
+twitter_worth = "7_000_000_000"
+age = 18
+cost = 1945.77
+is_rich = True
+is_false = "100"
 
-# integer -> float
-# fees = 1000
-# fees_float = float(fees)
-# print(fees_float)
+# data_type = type(twitter_ceo)
+is_rich_str = str(is_rich)
+print(type(is_rich_str))
+
+float_age = float(age)
+print(float_age)
+# is_false_bool = bool(is_false)
+# print(is_false_bool)
+# conversion of string to int
+# int_musk = int(cost)
+# str_age = str(age)
+# str_cost = str(cost)
+
+# print(type(str_cost))
+# print(type(str_age))
+# print(int_musk)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # programming_lng = input("What is your favorite programming language?")
 # print(type(programming_lng))
@@ -85,14 +131,14 @@ docstring is used for multiline commenting
 # is_good = input("IS this python course great? True or False")
 # print(type(is_good))
 # print(is_good)
-name = input("what is your name? ")
-dream_company = input("What is the name of your dream company? ")
-programming_lng = input("what is your favorite programming language? ")
-# I am philip, i wish to work at amazon and my favorite programming language is python
-# description = "I am " + name + ", i wish to work at " + dream_company + "and my favorite programming language is " + programming_lng
-description = f"I am {name}, i wish to work at {dream_company} and my favorite programming language is {programming_lng} "
-# string concatenation
-print(description)
+# name = input("what is your name? ")
+# dream_company = input("What is the name of your dream company? ")
+# programming_lng = input("what is your favorite programming language? ")
+# # I am philip, i wish to work at amazon and my favorite programming language is python
+# # description = "I am " + name + ", i wish to work at " + dream_company + "and my favorite programming language is " + programming_lng
+# description = f"I am {name}, i wish to work at {dream_company} and my favorite programming language is {programming_lng} "
+# # string concatenation
+# print(description)
 
 
 
